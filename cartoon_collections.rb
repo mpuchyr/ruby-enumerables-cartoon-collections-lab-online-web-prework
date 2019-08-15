35f36be89a2fc68e6a9cb55814cb1231cba2859a
Matt Puchyr <matt.puchyr@gmail.com>
2019-08-15 22:00:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ def long_planeteer_calls(short_words)
     longer_than_four
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(ingredients)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
